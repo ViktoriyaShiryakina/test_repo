fb89ef13a4371aecafd88457162f8480828ecb99
ViktoriyaShiryakina <56593251+ViktoriyaShiryakina@users.noreply.github.com>
2019-12-07 19:15:30 +0300

    Update пояснения к ДЗ1.sql
    Final homework_1

--- пояснения к ДЗ1.sql
+++ пояснения к ДЗ1.sql
@@ -1,28 +1,28 @@
--- К заданию 1
+-- ЗАДАНИЕ 1
 -- Переделываем все 3 документа в формат csv и создаем таблицы в postgresql
--- скрипт для таблицы listing_task
+-- скрипт для таблицы listing
 
-DROP TABLE IF EXISTS public.listing_task;
+DROP TABLE IF EXISTS public.listing;
 
-CREATE TABLE public.listing_task
+CREATE TABLE public.listing
 (
     "ID" integer NOT NULL,
     "ISIN" text NOT NULL,
     "PLATFORM" text NOT NULL,
     "SECTION" text NOT NULL,
-    CONSTRAINT listing_task_pkey PRIMARY KEY ("ID")
+    CONSTRAINT listing_pkey PRIMARY KEY ("ID")
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public.listing_task
+ALTER TABLE public.listing
     OWNER to postgres;
 	
---Создание таблицы public.quotes_task:
+--Создание таблицы public.quotes:
 
-DROP TABLE IF EXISTS public.quotes_task;
+DROP TABLE IF EXISTS public.quotes;
 
-CREATE TABLE public.quotes_task
+CREATE TABLE public.quotes
 (
     "ID" integer NOT NULL,
     "TIME" date NOT NULL,
@@ -62,14 +62,14 @@ CREATE TABLE public.quotes_task
 
 TABLESPACE pg_default;
 
-ALTER TABLE public.quotes_task
+ALTER TABLE public.quotes
     OWNER to postgres;
 
--- Создание таблицы public.bond_description_task
+-- Создание таблицы public.bond_description
 
-DROP TABLE IF EXISTS public.bond_description_task;
+DROP TABLE IF EXISTS public.bond_description;
 
-CREATE TABLE public.bond_description_task
+CREATE TABLE public.bond_description
 (
     "ISIN_REGCODE_NRDCODE" text NOT NULL,
     "FIN_TOOL_TYPE" text NOT NULL,
@@ -122,107 +122,119 @@ CREATE TABLE public.bond_description_task
     "ISSUER_OKPO" integer,
     "NUM_GUARANTORS" smallint NOT NULL,
     "END_MTY_DATE" date,
-    CONSTRAINT bond_description_task_pkey PRIMARY KEY ("ISIN_REGCODE_NRDCODE")
+    CONSTRAINT bond_description_pkey PRIMARY KEY ("ISIN_REGCODE_NRDCODE")
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public.bond_description_task
+ALTER TABLE public.bond_description
     OWNER to postgres;
 
--- Импортируем данные из файла csv в postgresql (таблица listing_task)
-\copy public.listing_task  FROM 'C:\Users\USER\Desktop\IT для финансистов\ДЗ №1\Data\Облигации\listing_task.csv' 
-DELIMITER ';' CSV HEADER;
--- Импортируем данные из файла csv в postgresql (таблица bond_description_task)
-\copy public.bond_description_task  FROM 'C:\Users\USER\Desktop\IT для финансистов\ДЗ №1\Data\Облигации\bond_description_task.csv' 
-DELIMITER ';' CSV HEADER;
--- Импортируем данные из файла csv в postgresql (таблица quotes_task)
-\copy public.quotes_task  FROM 'C:\Users\USER\Desktop\IT для финансистов\ДЗ №1\Data\Облигации\quotes_task.csv' 
-DELIMITER ';' CSV HEADER;
---Замечание: в процессе импорта данных будет неоднократно требоваться поменять форматы определенных столбцов в csv-файле.
---Удаление лишних столбцов из таблицы public.bond_description_task (тех, которые были скрыты в изначальном файле excel):
-ALTER TABLE public.bond_description_task
-DROP COLUMN "ISLOMBARDCBR_NRD", 
-DROP COLUMN "ISQUALIFIED_NRD",
-DROP COLUMN "FORMARKETBONDS_NRD",
-DROP COLUMN "MICEXLIST_NRD",
-DROP COLUMN "BASIS",
-DROP COLUMN "BASIS_NRD",
-DROP COLUMN "BASE_MONTH",
-DROP COLUMN "BASE_YEAR",
-DROP COLUMN "COUPON_PERIOD_BASE_ID",
-DROP COLUMN "ACCRUEDINTCALCTYPE",
-DROP COLUMN "ISGUARANTEED",
-DROP COLUMN "GUARANTVAL",
-DROP COLUMN "SECURITIZATION",
-DROP COLUMN "COUPONPERYEAR",
-DROP COLUMN "CP_TYPE_ID", 
-DROP COLUMN "NUMCOUPONS",
-DROP COLUMN "NUMCOUPONS_M",
-DROP COLUMN "NUMCOUPONS_NRD",
-DROP COLUMN "COUNTRY",
-DROP COLUMN "FACEFTNAME", 
-DROP COLUMN "FACEFTNAME_M", 
-DROP COLUMN "FACEFTNAME_NRD", 
-DROP COLUMN "FACEVALUE", 
-DROP COLUMN "FACEVALUE_M", 
-DROP COLUMN "FACEVALUE_NRD", 
-DROP COLUMN "CURRENTFACEVALUE_NRD";
+-- Импортируем данные из файла csv в postgresql (таблица listing)
+\copy public.listing FROM 'C:\Users\USER\Desktop\IT для финансистов\ДЗ1_NEW\listing.csv' DELIMITER ';' CSV HEADER;
 
---К заданию 2.
---Добавляем в таблицу listing_task полей с кодами-сылками на таблицу bond_description_task и quotes_task:
-ALTER TABLE listing_task ADD COLUMN "ISSUER_NAME" text, 
+-- Импортируем данные из файла csv в postgresql (таблица bond_description)
+-- предварительно нужно переделать формат данных в полях HAVEOFFER и AMORTISEDMTY в формат "число" в Ms Excel, а в 133 строке вообще сделать это вручную, заменить на 0.
+\copy public.bond_description FROM 'C:\Users\USER\Desktop\IT для финансистов\ДЗ1_NEW\bond_description.csv' DELIMITER ';' CSV HEADER;
+
+-- Импортируем данные из файла csv в postgresql (таблица quotes). Предварительно нужно переделать поля TIME и BUYBACKDATE в форма "Дата" в Ms Excel;
+\copy public.quotes FROM 'C:\Users\USER\Desktop\IT для финансистов\ДЗ1_NEW\quotes.csv' DELIMITER ';' CSV HEADER;
+
+--ЗАДАНИЕ 2.
+--Добавляем в таблицу listing поля с кодами-сылками на таблицу bond_description и quotes:
+ALTER TABLE listing ADD COLUMN "ISSUER_NAME" text, 
 ADD COLUMN "ISSUER_NAME_NRD" text, 
 ADD COLUMN "ISSUER_OKPO" integer, 
 ADD COLUMN "BOARDID" text, 
-ADD COLUMN "BOARDNAME" text,
+ADD COLUMN "BOARDNAME" text;
 
---заполнение полей с кодами-ссылками на таблицу bond_description_task:
+--заполнение полей с кодами-ссылками на таблицу bond_description:
 update listing_task
 set("ISSUER_NAME","ISSUER_NAME_NRD","ISSUER_OKPO")=(SELECT "ISSUER_NAME","ISSUER_NAME_NRD","ISSUER_OKPO"
 									 from bond_description_task
 									 where listing_task."ISIN"=bond_description_task."ISIN_CODE");
--- Создаем новую таблицу, чтобы связать данные для добавления в таблицу listing_task полей из таблицы quotes_task:
-DROP TABLE IF EXISTS public.exchange;
+									 
+--Представленный ниже способ занимает у меня очень долгое время (попросту не грузит), и поэтому лучше идти в обход через вынесение данных в отдельную таблицу:
+update listing 
+set ("BOARDID", "BOARDNAME")=(select distinct "BOARDID","BOARDNAME" from quotes where listing."ID"=quotes."ID";
 
-CREATE TABLE public.exchange
+-- Создаем новую таблицу, чтобы связать данные для добавления в таблицу listing полей из таблицы quotes:
+DROP TABLE IF EXISTS public.board_info;
+
+CREATE TABLE public.board_info
 (
-    exchange_order bigint NOT NULL,
     "ID" bigint NOT NULL,
-    "BOARDID" text COLLATE pg_catalog."default",
-    "BOARDNAME" text COLLATE pg_catalog."default"
+    "BOARDID" text,
+    "BOARDNAME" text,
+    CONSTRAINT board_info_pkey PRIMARY KEY ("ID")
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public.exchange
+ALTER TABLE public.board_info
     OWNER to postgres;
 
---вынесение данных в новую таблицу exchange из таблицы quotes_task:
-insert into exchange select count(*) over (order by "ID", "BOARDID","BOARDNAME") as exchange_order,
-"ID", "BOARDID","BOARDNAME"
-from (select distinct "ID", "BOARDID","BOARDNAME"
-from quotes_task)
- as my_select;
+--вынесение данных в новую таблицу boardinfo из таблицы quotes:
+insert into board_info
+select distinct "ID", "BOARDID","BOARDNAME"
+from quotes;
  
---заполнение полей с кодами-ссылками на новую таблицу exchange
-update listing_task
+--заполнение полей с кодами-ссылками на новую таблицу boardinfo
+update listing
 set("BOARDID","BOARDNAME")=(SELECT "BOARDID","BOARDNAME"
-									 from exchange
-									 where listing_task."ID"=exchange."ID");
---К ЗАДАНИЮ 3.
---Присваиваем первичный ключ таблице exchange:
-ALTER TABLE exchange ADD PRIMARY KEY ("exchange_order");
+									 from board_info
+									 where listing."ID"=board_info."ID");
+									 
+--ЗАДАНИЕ 3.
+--Чтобы связать таблицы quotes и listing через внешний ключ, нужно сначала внести в поле ID таблицы listing,которое является для нее первичным ключом,
+недостающие значения из таблицы quotes:
+--НО! Предварительно снимаем требование null с полей ISIN,PLATFORM и SECTION в таблице listing:
+insert into listing ("ID", "ISIN", "BOARDID", "BOARDNAME")
+select distinct quotes."ID", quotes."ISIN", quotes."BOARDID", quotes."BOARDNAME"
+from quotes left join listing on quotes."ID"=listing."ID"
+where listing."ID" is null;
 
--- присвоение полю ограничение внешнего ключа
-ALTER TABLE public.listing_task
-ADD CONSTRAINT fr_key_1 FOREIGN KEY ("exchange_order") REFERENCES public.exchange ("exchange_order");
+--Затем присваиваем внешний ключ:
+ALTER TABLE quotes
+ADD CONSTRAINT fr_key_1 FOREIGN KEY ("ID") REFERENCES listing ("ID");
 
---К ЗАДАНИЮ 4 (написано только начало кода)
-select "ISIN", count ("TIME") as day_missed
-from quotes_task
-where "BOARDNAME"='ЦК - Режим основных торгов' and "ASK"=0
-group by "ISIN";
+-- Теперь, для того, чтобы связать таблицы listing и bond_description, нужно в таблице bond_description создать поле ID (по этому полю будем связывать эти таблицы):
+alter table bond_description
+add column "ID" integer;
+
+-- Импортируем в это поле значения ID из таблицы listing:
+UPDATE bond_description
+SET "ID"=listing."ID"
+FROM listing
+WHERE bond_description."ISIN, REGCODE, NRDCODE"=listing."ISIN";
+
+-- Присваиваем внешний ключ для таблицы bond_description:
+ALTER TABLE bond_description
+ADD CONSTRAINT fr_key_1 FOREIGN KEY ("ID") REFERENCES listing ("ID");
+
+
+--ЗАДАНИЕ 4 
+with quotes_1 as (
+-- считаем, сколько всего было наблюдений за бумагами, удовлетворяющих условию торговли на МБ в режиме основных торгов 
+	select listing."ISIN", listing."ISSUER_NAME", count (quotes."TIME") as gen_obs
+	from quotes inner join listing
+	on listing."ID"=quotes."ID"
+	where listing."SECTION"=' Основной' and listing."PLATFORM"='Московская Биржа '
+	group by listing."ISIN", listing."ISSUER_NAME"
+), quotes_2 as (
+--считаем кол-во дней, когда котировка ASK не существовала или была бы равна 0 среди тех облигаций, которые торгуются на МБ в режиме основных торгов
+	select listing."ISIN", listing."ISSUER_NAME", count (quotes."TIME") as day_missed
+	from quotes inner join listing
+	on listing."ID"=quotes."ID"
+	where listing."SECTION"=' Основной' and listing."PLATFORM"='Московская Биржа '
+	and (quotes."ASK" is not null and quotes."ASK">0)
+	group by listing."ISIN", listing."ISSUER_NAME")
+select quotes_2."ISIN",
+quotes_2."ISSUER_NAME",
+(quotes_2.day_missed::real / quotes_1.gen_obs::real) as nun_ratio
+from quotes_1 inner join quotes_2 
+on quotes_1."ISIN"=quotes_2."ISIN"
+where (quotes_2.day_missed::real / quotes_1.gen_obs::real)>0.9;
 
 
 
